c2924b87d397d657dbded81366d029e3bca35668
LukasInnerhofer <l.innerhofer@palfinger.com>
2021-11-03 11:26:45 +0100

    update

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 #include "libcommandline/flag.h"
-#include "libcommandline/interface.h"
+#include "libcommandline/parser.h"
 
 int main(int argc, char const *argv[])
 {
-    auto f{LibUtilities::makeNonNullShared<LibCommandLine::Flag>('f')};
-    auto g{LibUtilities::makeNonNullShared<LibCommandLine::Flag>('g')};
-    LibCommandLine::addOption(f);
-    LibCommandLine::addOption(g);
-    LibCommandLine::parse(argc, argv);
+    auto f{makeNonNullShared<LibCommandLine::Flag>('f')};
+    auto g{makeNonNullShared<LibCommandLine::Flag>('g')};
+    LibCommandLine::Parser::addOption(f);
+    LibCommandLine::Parser::addOption(g);
+    LibCommandLine::Parser::parse(argc, argv);
     if (*f)
     {
         std::cout << "-f is set\n";
