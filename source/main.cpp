fb102ce831703d34490ee35d7a6ae62c6fad3299
LukasInnerhofer <l.innerhofer@palfinger.com>
2021-11-03 13:15:39 +0100

    Added multi option

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
 #include "libcommandline/flag.h"
+#include "libcommandline/multi_option.h"
 #include "libcommandline/parser.h"
 
 int main(int argc, char const *argv[])
 {
     auto f{makeNonNullShared<LibCommandLine::Flag>('f')};
     auto g{makeNonNullShared<LibCommandLine::Flag>('g')};
+    auto i{makeNonNullShared<LibCommandLine::MultiOption>('i')};
     LibCommandLine::Parser::addOption(f);
     LibCommandLine::Parser::addOption(g);
+    LibCommandLine::Parser::addOption(i);
     LibCommandLine::Parser::parse(argc, argv);
     if (*f)
     {
@@ -17,5 +20,14 @@ int main(int argc, char const *argv[])
     if (*g)
     {
         std::cout << "-g is set\n";
+    }
+    auto args = i->getArguments();
+    if (!args.empty())
+    {
+        std::cout << "-i passed with arguments:\n";
+        for (auto arg : args)
+        {
+            std::cout << arg << "\n";
+        }
     }
 }
